dd9f12c015fa671b61ba6fbfc1e3375567e5536a
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-04 01:30:45 +0530

    Add files via upload

---
+++ JavaBasics.java
@@ -0,0 +1,9 @@
+public class JavaBasics{
+    public static void main(String args[]){
+
+
+
+
+    }
+}
+// Boilerplate code
---
+++ Output.java
@@ -0,0 +1,8 @@
+public class Output {
+    public static void main(String args[]){
+
+        System.out.println("Hello World");    // line ka space
+        System.out.println("Hello World");    // line ka space
+        System.out.print("Hello World");
+    }
+}
