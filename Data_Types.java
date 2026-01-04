ab1faa2624be3229b9c9e2052b4963c8cb93ed26
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-04 21:19:56 +0530

    Add files via upload

---
+++ Data_Types.java
@@ -0,0 +1,44 @@
+// Primitive:- Jo already exist karta hai java ke andar jinhe hume khudse banana nai padta (Byte, short, char, boolean, int, long, float, double)
+
+// Non-Primitive:- Hume khudse banana padtay means hume khudse create karna padtay unko use karne keliye.(String, Array, Class, Object, Interface)
+
+
+public class Data_Types{
+    public static void main(String args []){
+
+        int number = 20;
+        System.out.println(number);
+
+        float price = 5.1f;
+        System.out.println(price);
+
+        double n = 1323434.212;
+        System.out.println(n);
+
+        byte b = 8;
+        System.out.println(b);
+
+        long num = 123242332L;
+        System.out.println(num);
+
+        short s = 240;
+        System.out.println(s);
+
+        boolean var = true;
+        System.out.println(var);
+
+        char ch = 's';
+        System.out.print(ch);
+
+    }
+}
+
+
+// Size of Data Types:- 
+// byte = 1 byte, short = 2 bytes, char = 2 bytes, boolean = 1 byte, int = 4 bytes, long = 8 bytes, float = 4 bytes, double = 8 bytes
+
+// byte : [-128 to 127] Total 256 bytes
+// char : ['a' to 'z' 'A' to 'Z' '@' '%' ',']
+// boolean : true, false
+// int : -2B --- +2B
+
