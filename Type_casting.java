256239a6cef1d709484260a2fce4fde228da7f83
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-05 00:53:16 +0530

    Add files via upload

---
+++ Type_casting.java
@@ -0,0 +1,15 @@
+// Type Casting is also called as narrowing and Explicit Conversion.
+
+import java.util.*;
+
+public class Type_casting{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = 2.55f;
+        int b = (int) a;
+        System.out.println(b);
+
+        sc.close();
+    }
+}
+
