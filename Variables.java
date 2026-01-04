afd15427607d3a4dcc7d0acac6e70f30c77b273f
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-04 20:33:41 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        
+        int a = 5;
+        int b = 6;
+
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Sarvesh";
+        System.out.println(name);
+
+        a = 10;
+        System.out.print(a + b);
+    }
+}
+
+// a & b are identifiers hence its a variable.
