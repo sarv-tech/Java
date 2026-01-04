c0d107be2fd974e1e9054eae613184b4fe2e7adb
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-05 00:26:05 +0530

    Add files via upload

---
+++ Type_conversion.java
@@ -0,0 +1,22 @@
+// Type Conversion is also called as widening and Implicit Conversion.
+
+/* 
+
+Conversion happens when:
+a. type compatible
+b. destination type > source type
+
+byte --> short --> int --> float --> long --> double
+
+ */
+
+
+import java.util.*;
+
+public class Type_conversion{
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        float a = sc.nextInt();
+        System.out.println(a);
+    }
+}
