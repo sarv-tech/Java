3a83c129975b54b8dd5205bf7032d5f52d04b1bb
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-05 01:15:50 +0530

    Add files via upload

---
+++ Type_promotion.java
@@ -0,0 +1,7 @@
+/*
+Type Promotion in Expressions:-
+
+1. Java automatically promotes each byte, short, or char operand to int when evaluating an expression.
+2. If one operand is long, float or double the whole expression is promoted to long, float, or double respectively.
+
+*/
