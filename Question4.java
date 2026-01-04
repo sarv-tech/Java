779ce59a1fb34e223483598b6708f192d004ccbb
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-05 02:32:09 +0530

    Add files via upload

---
+++ Question4.java
@@ -0,0 +1,10 @@
+/*
+Q. 
+Will the following statementgive any error in Java?
+int $ = 24;
+
+*/
+
+
+
+// No, the statement will not give any error. Names of variables are called identifiers in Java. Identifier rule says, identifiers can start with any alphabet or underscore (“_”) or dollar (“$”). According to the rule the given variable name is a valid identifier.
