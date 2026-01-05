cd5584d72b35bf6422b24fa29fde155b54fe63a4
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-06 00:55:21 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,19 @@
+/* Operators in Java:-
+
+Symbols that tell compiler to perform some operation.
+
+sum = a + b
+a,b => operands, + => operator
+
+
+
+Types of Operators:-
+
+Arithmetic Operators (Binary / Unary)
+Relational Operators
+Logical Operators
+Bitwise Operators (Bit Manipulation)
+Assignment Operators
+Ternary Operators
+
+*/
