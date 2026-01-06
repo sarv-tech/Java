87d46a4cc5d9273254d9abe6dcae0fc952ed10de
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-07 01:29:08 +0530

    Add files via upload

---
+++ Question5.java
@@ -0,0 +1,16 @@
+// Q. Print the largest of 2 numbers
+
+public class Question5 {
+    public static void main(String[] args) {
+
+        int A = 10;
+        int B = 5;
+
+        if(A >= B){
+            System.out.println("A is largest of 2");
+        }
+        else{
+            System.out.println("B is largest of 2");
+        }
+    }
+}
