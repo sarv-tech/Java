57d530b4ad8b9c40899db339cdfa0f8e4541cbd9
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-07 02:10:29 +0530

    Add files via upload

---
+++ Question7.java
@@ -0,0 +1,26 @@
+// Q. Income Tax Calculator
+
+import java.util.*;
+
+public class Question7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tax;
+        System.out.print("Enter an Income: ");
+        int income = sc.nextInt();
+
+        if(income < 500000){
+            tax = 0;
+        }
+        else if(income >= 500000 && income < 1000000){
+            tax = (int) (income * 0.2);
+        }
+        else{
+            tax = (int) (income * 0.3);
+        }
+        System.out.println("Your Tax is: " + tax);
+
+        sc.close();
+    }
+}
