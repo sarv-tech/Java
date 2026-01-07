bb95b353e3063b2423e2f3a450d2de98bd934424
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-08 01:33:27 +0530

    Add files via upload

---
+++ Question12.java
@@ -0,0 +1,17 @@
+// Q. WAP that prints You have a fever if your temperature is above 100 and otherwise prints You don't have a fever.
+
+
+
+public class Question12 {
+    public static void main(String args[]) {
+
+        double temp = 103.5;
+
+        if (temp > 100) {
+            System.out.println("OMG! You have a fever");
+        } 
+        else {
+            System.out.println("Relax! You don't have a fever");
+        }
+    }
+}
