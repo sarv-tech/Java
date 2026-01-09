ee24a2c0d4f87de81431a7a37d219ae671688fea
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-09 21:19:15 +0530

    Add files via upload

---
+++ While_loop.java
@@ -0,0 +1,32 @@
+// While Loop:-
+
+
+/*  Syntax:-
+
+while(condition){
+
+// do something
+
+
+}
+
+*/
+
+
+// Print Hello World 100 times
+package Loops;
+
+public class While_loop{
+    public static void main(String[] args) {
+        
+        int count = 1;      // Initialize
+
+        while(count <= 100){
+
+            System.out.println("Hello World \t"+ (count));
+
+            count++;
+            
+        }
+    }
+}
