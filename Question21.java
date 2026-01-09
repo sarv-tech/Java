2c41aac96941369449b55179d78473c428ad88ca
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-10 03:00:36 +0530

    Add files via upload

---
+++ Question21.java
@@ -0,0 +1,22 @@
+// Q. WAP that keep entering numbers till user enters a multiple of 10.
+
+import java.util.*;
+
+public class Question21 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter your number: ");
+            int n = sc.nextInt();
+            
+            if(n % 10 == 0) {
+                break;
+            }
+            System.out.println(n);
+
+        } while (true);
+
+    }
+}
