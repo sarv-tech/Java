f1a44b60ea7da2f816348d987db998cc671211c7
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-09 21:41:23 +0530

    Add files via upload

---
+++ Question15.java
@@ -0,0 +1,15 @@
+// Q. Print number from 1 to 10
+
+
+public class Question15 {
+    public static void main(String[] args) {
+        
+        int count = 1;      // initialize
+
+        while(count <= 10) {
+
+            System.out.println(count);
+            count++;
+        }
+    }
+}
