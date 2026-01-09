957e2e45a85b5b0f3b7b3079eb58f0ff8f45c7b5
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-10 02:36:20 +0530

    Add files via upload

---
+++ do_while_loop.java
@@ -0,0 +1,29 @@
+// do-while Loop:-
+
+/* Syntax:-
+
+do {
+
+do something
+
+
+}while(condition);
+
+*/
+
+
+// Print Hello World 10 times using do-while loop
+package Loops;
+
+public class do_while_loop {
+    public static void main(String[] args) {
+
+        int count = 1;
+        
+        do { 
+            System.out.println("Hello World");
+            count++;
+            
+        } while (count <= 10);
+    }
+}
