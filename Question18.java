440b44915f93381f6124716c961271bcc67b27e9
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-10 00:27:17 +0530

    Add files via upload

---
+++ Question18.java
@@ -0,0 +1,21 @@
+// Q. Print Square Pattern
+
+/* 4 stars
+
+*  *  *  *     ---> L1
+*  *  *  *     ---> L2
+*  *  *  *     ---> L3
+*  *  *  *     ---> L4
+
+*/
+
+
+public class Question18 {
+    public static void main(String[] args) {
+        
+        for(int line = 1; line <= 4; line++) {
+
+            System.out.println("*  *  *  *");
+        }
+    }
+}
