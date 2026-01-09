4faa244c13db65067926fc4efd82faccab5e05bb
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-09 20:50:56 +0530

    Add files via upload

---
+++ Basic_loops.java
@@ -0,0 +1,26 @@
+// Loops in programming allow a set of instructions to run multiple times based on a condition.
+
+
+package Loops;
+
+public class Basic_loops {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+        System.out.println("Hell World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+    }
+}
+
+
+/*
+
+=> While loop
+
+=> do While loop
+
+=> for loop
+
+*/
