71c03572540f1b8a713c40309f4a285758e54b88
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-10 00:15:19 +0530

    Add files via upload

---
+++ for_loops.java
@@ -0,0 +1,30 @@
+// for Loop:-  Java for loop is a control flow statement that allows code to be executed repeatedly based on a given condition. The for loop in Java provides an efficient way to iterate over a range of values, execute code multiple times, or traverse arrays and collections.
+
+
+/* Syntax:-
+
+for(initialisation; condittion; updation) {
+
+do something
+
+
+}
+
+
+*/
+
+
+
+// Print Hello World 10 times using for Loop
+package Loops;
+
+public class for_loops{
+    public static void main(String[] args) {
+        
+        for(int i = 0; i < 10; i++) {
+
+            System.out.println("Hello World");
+        }
+    }
+
+}
