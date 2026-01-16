16b12e509614b551de40963af1fb92cd619daa6b
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-17 01:14:12 +0530

    Add files via upload

---
+++ method_types.java
@@ -0,0 +1,13 @@
+// Type of Methods:-
+
+/*
+
+1. user defined - joh hum khudse likhte arhe
+eg.:- factorial, sum, product
+
+2. in-built - joh pehle se hi java ke andar exist karte hai
+eg.:- math - pow, sqrt, max
+sc.nextInt
+
+
+*/
