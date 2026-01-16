0211f56b32cf8198e7a74e2497c47cbadd0c1dfa
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-17 01:13:15 +0530

    Add files via upload

---
+++ Question45.java
@@ -0,0 +1,27 @@
+// Q. WAP to swap the values of number.
+
+// java always call by value
+
+public class Question45 {
+    
+    public static void swap(int a, int b) {
+
+        // swap
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+    public static void main(String args[]) {
+
+        // swap - values exchange
+
+        int a = 5;
+        int b = 10;
+        swap(a, b);
+
+    }
+}
