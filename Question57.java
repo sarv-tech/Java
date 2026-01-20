48523db32dbdd1fd7eff1a1f67cde20cf4ac8c3b
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-21 01:18:01 +0530

    Add files via upload

---
+++ Question57.java
@@ -0,0 +1,39 @@
+// Q. WAP to Print HOLLOW RHOMBUS Pattern 
+
+/*
+
+    *****
+   *   *
+  *   *
+ *   *
+*****
+
+
+*/
+
+
+public class Question57 {
+    public static void main(String args[]) {
+
+        for(int i = 1; i <= 5; i++) {
+
+            for(int j = 1; j <= (5 - i); j++) {
+
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= 5; j++) {
+
+                if(i == 1 || i == 5 || j == 1 || j == 5) {
+
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
