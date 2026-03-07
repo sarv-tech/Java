28c8f0fef253726b6e423a201526ab49abe0bc9b
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-03-08 01:34:54 +0530

    Add files via upload

---
+++ whatIsStrings.java
@@ -0,0 +1,16 @@
+// Sometimes it can happen that we need to store some text in our memory, for example a name, words, a sentence, or even a whole paragraph. In such cases, we use a data structure called "string", in which we can store characters, numbers, or anything that can be represented in the form of text.
+
+// Strings => WORDS, PARAGRAPHS, SENTENCES
+
+
+public class whatIsStrings {
+
+    public static void main(String[] args) {
+
+        char arr[] = {'a', 'b', 'c', 'd'};
+        String str = "abcd";
+        String str2 = new String("xyz");
+
+        // Strings are IMMUTABLE
+    }
+}
